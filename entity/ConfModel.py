6dbf6346bd6a924773cc5107041bd96461155d10
Ze8c <Ze8c.Max@gmail.com>
2024-05-02 00:54:43 +0200

    Change init ConfModel

--- entity/ConfModel.py
+++ entity/ConfModel.py
@@ -2,7 +2,5 @@ class ConfModel:
 
     def __init__(self, file_info: str):
         tmp_dict = eval(file_info)
-        self.host = tmp_dict['server']
-        self.port = int(tmp_dict['port'])
-        self.user = tmp_dict['email']
-        self.password = tmp_dict['pass']
+        self.host, self.port = tmp_dict['server'], int(tmp_dict['port'])
+        self.user, self.password = tmp_dict['email'], tmp_dict['pass']
